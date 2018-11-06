aa5b5efff8c450ab2a6e87c3617f5fdcc9adab7d
Steven Berk <steven.e.berk@gmail.com>
2018-11-06 11:41:33 -0500

    dom elements added to index js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
+var output = document.querySelector(".output");
+var movieGenre = document.querySelector(".movie-genre");
+
 $.ajax({
     type: "GET",
-    url: "http://www.omdbapi.com/?i=tt3896198&apikey=6c8e631&t=black_mass",
+    url: "http://www.omdbapi.com/?i=tt3896198&apikey=6c8e631&t=mystic_river",
     success: function(movie){
         console.log(movie.Genre)
+        movieGenre.textContent = movie.Genre;
+        output.setAttribute('class', 'show output');
+        
     },
     error: function(error){
         console.log(error)
