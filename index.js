6a1f07d0930430b06b12755504ee6b513190f45f
Steven Berk <steven.e.berk@gmail.com>
2018-11-08 11:16:45 -0500

    form field works

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 var output = document.querySelector(".output");
 var movieGenre = document.querySelector(".movie-genre");
 
+var form = document
+  .querySelector(".form")
+  .addEventListener("submit", function(e) {
+    e.preventDefault();
+    var input = document.querySelector(".main-input");
+    getMoviesAPI(input.value);
+  });
+
+
+let getMoviesAPI = (value) => {
 $.ajax({
     type: "GET",
-    url: "http://www.omdbapi.com/?i=tt3896198&apikey=6c8e631&t=mystic_river",
+    url: `http://www.omdbapi.com/?i=tt3896198&apikey=6c8e631&t=${value}`,
     success: function(movie){
         console.log(movie.Genre)
         movieGenre.textContent = movie.Genre;
@@ -15,3 +25,4 @@ $.ajax({
         console.log(error)
     }
 })
+}
