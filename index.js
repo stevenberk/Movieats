77721cb3ae74ec3cf2b0ddc4e9a3ca62efc4a43d
Steven Berk <steven.e.berk@gmail.com>
2018-11-26 09:51:34 -0500

    all sets of if statements consolidated into one set and put inside of a loop

--- index.js
+++ index.js
@@ -37,181 +37,58 @@ $.ajax({
                 recipeGenrePoints = recipeGenrePoints + 5 
                 console.log("bio loop" + recipeGenrePoints)
             }
+            if (splitgenre[i] == "Documentary"){
+                recipeGenrePoints = recipeGenrePoints + 6
+                console.log("doc loop" + recipeGenrePoints)
+            }
+           if (splitgenre[i]  == "Comedy"){
+                recipeGenrePoints = recipeGenrePoints + 6 
+                console.log("comedy" + recipeGenrePoints)
+            }
+            if (splitgenre[i]  == "Drama"){
+                recipeGenrePoints = recipeGenrePoints + 5 
+                console.log("drama" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Animation"){
+                recipeGenrePoints = recipeGenrePoints + 3 
+                console.log("amination" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Adventure"){
+                recipeGenrePoints = recipeGenrePoints + 3 
+                console.log("adventure" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Romance"){
+                recipeGenrePoints = recipeGenrePoints + 3 
+                console.log("romance" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Action"){
+                recipeGenrePoints = recipeGenrePoints + 2 
+                console.log("action" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Sport"){
+                recipeGenrePoints = recipeGenrePoints - 2 
+                console.log("sport" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Fantasy"){
+                recipeGenrePoints = recipeGenrePoints - 1 
+                console.log("fantasy" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Family"){
+                recipeGenrePoints = recipeGenrePoints - 1 
+                console.log("family" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Short"){
+                recipeGenrePoints = recipeGenrePoints + 3 
+                console.log("short" + recipeGenrePoints)
+            }
+            if (splitgenre[i] == "Music"){
+                recipeGenrePoints = recipeGenrePoints + 3 
+                console.log("music" + recipeGenrePoints)
+            }
        }
        console.log(splitgenre);
 
-    
 
-
-
-//index value 0
-       if (splitgenre[0] == "Documentary"){
-            recipeGenrePoints = recipeGenrePoints + 6
-       }
-        if (splitgenre[0]  == "Crime"){
-        recipeGenrePoints = recipeGenrePoints + 5
-        console.log("crime" + recipeGenrePoints)
-        }
-        if (splitgenre[0]  == "Comedy"){
-            recipeGenrePoints = recipeGenrePoints + 6 
-            console.log("comedy" + recipeGenrePoints)
-        }
-        if (splitgenre[0]  == "Drama"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("drama" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Animation"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("amination" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Adventure"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("adventure" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Romance"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("romance" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Action"){
-            recipeGenrePoints = recipeGenrePoints + 2 
-            console.log("action" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Biography"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("bio" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Sport"){
-            recipeGenrePoints = recipeGenrePoints - 2 
-            console.log("sport" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Fantasy"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("fantasy" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Family"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("family" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Short"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("short" + recipeGenrePoints)
-        }
-        if (splitgenre[0] == "Music"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("music" + recipeGenrePoints)
-        }
-// index value 1
-        if (splitgenre[1] == "Documentary"){
-            recipeGenrePoints = recipeGenrePoints + 6
-       }
-        if (splitgenre[1]  == "Crime"){
-        recipeGenrePoints = recipeGenrePoints + 5
-        console.log("crime" + recipeGenrePoints)
-        }
-        if (splitgenre[1]  == "Comedy"){
-            recipeGenrePoints = recipeGenrePoints + 6 
-            console.log("comedy" + recipeGenrePoints)
-        }
-        if (splitgenre[1]  == "Drama"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("drama" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Animation"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("amination" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Adventure"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("adventure" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Romance"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("romance" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Action"){
-            recipeGenrePoints = recipeGenrePoints + 2 
-            console.log("action" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Biography"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("bio" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Sport"){
-            recipeGenrePoints = recipeGenrePoints - 2 
-            console.log("sport" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Fantasy"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("fantasy" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Family"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("family" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Short"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("short" + recipeGenrePoints)
-        }
-        if (splitgenre[1] == "Music"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("music" + recipeGenrePoints)
-        }
-//index value 2
-        if (splitgenre[2] == "Documentary"){
-            recipeGenrePoints = recipeGenrePoints + 6
-        }
-        if (splitgenre[2]  == "Crime"){
-        recipeGenrePoints = recipeGenrePoints + 5
-        console.log("crime" + recipeGenrePoints)
-        }
-        if (splitgenre[2]  == "Comedy"){
-            recipeGenrePoints = recipeGenrePoints + 6 
-            console.log("comedy" + recipeGenrePoints)
-        }
-        if (splitgenre[2]  == "Drama"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("drama" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Animation"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("amination" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Adventure"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("adventure" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Romance"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("romance" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Action"){
-            recipeGenrePoints = recipeGenrePoints + 2 
-            console.log("action" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Biography"){
-            recipeGenrePoints = recipeGenrePoints + 5 
-            console.log("bio" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Sport"){
-            recipeGenrePoints = recipeGenrePoints - 2 
-            console.log("sport" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Fantasy"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("fantasy" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Family"){
-            recipeGenrePoints = recipeGenrePoints - 1 
-            console.log("family" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Short"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("short" + recipeGenrePoints)
-        }
-        if (splitgenre[2] == "Music"){
-            recipeGenrePoints = recipeGenrePoints + 3 
-            console.log("music" + recipeGenrePoints)
-        }
        console.log(recipeGenrePoints);
         
     },
